b8acb2472deb238efce1c6687d871ad2f02e266c
greatSumini <greatSumini@gmail.com>
2021-09-21 06:16:00 +0900

    test: :white_check_mark: add queueService test

---
+++ src/services/queue.service.spec.ts
@@ -0,0 +1,31 @@
+import * as faker from 'faker';
+
+import { queueService } from './queue.service';
+
+describe('queueService', () => {
+  const setUp = () => {
+    const queueName = faker.lorem.word();
+    const body = {
+      Action: 'SendMessageBatch',
+      QueueUrl: `http://localhost:4413/896943254900/${queueName}`,
+      'SendMessageBatchRequestEntry.1.Id':
+        '2b6cc4e7-c102-e7eb-ab06-5f73534093b2',
+      'SendMessageBatchRequestEntry.1.MessageBody':
+        '{"id":11,"nickname":"최수민"}',
+      Version: '2012-11-05',
+    };
+
+    return { queueName, body };
+  };
+
+  it('should receive sent message', async () => {
+    const { queueName, body } = setUp();
+
+    queueService.sendBatch(queueName, body);
+    const result = await queueService.receive(queueName, {
+      WaitTimeSeconds: '0',
+    });
+
+    expect(result.Message.length).toEqual(1);
+  });
+});
