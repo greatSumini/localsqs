e32f572624a4f036e9bd53a126af054f2681b173
greatSumini <greatSumini@gmail.com>
2021-09-22 02:18:24 +0900

    feat: :wrench: add dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import Fastify from 'fastify';
+import dotenv from 'dotenv';
+
 import app from './app';
+
+dotenv.config();
 
 const port = Number(process.env.PORT) || 4413;
 const ip = process.env.IP || 'localhost';
