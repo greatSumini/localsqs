0033626f4e137fb278e2ed8c966f5594ca160902
greatSumini <greatSumini@gmail.com>
2021-09-23 13:42:26 +0900

    chore: :fire: remove fastify-cors
    CORS is not required because local-sqs is probably not accessed in a browser env

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import { FastifyInstance, FastifyPluginOptions } from 'fastify';
-import fastifyCors from 'fastify-cors';
 import fastifyFormBody from 'fastify-formbody';
 import { Server, IncomingMessage, ServerResponse } from 'http';
 
@@ -10,9 +9,6 @@ export default (
   _: FastifyPluginOptions,
   next: (error?: Error) => void
 ) => {
-  fastify.register(fastifyCors, {
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
   fastify.register(fastifyFormBody);
 
   // Middleware: Router
